5b92120bb6bd4ea103792b3080caa70cb84dd07e
Alex Gregg <agregg444@gmail.com>
2019-02-12 01:14:08 -0600

    Beginning game programming C++

---
+++ adventure_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+int main(){
+
+    std::string playerChoice = "";
+    std::cout << "Welcome to the land of Banvalza" << std::endl;
+    std::cin >> playerChoice;
+}
+
